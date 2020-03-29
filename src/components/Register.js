176f329c5658326864fc88d5d0a1059ff5904089
JB Wilson <53917177+Selenestica@users.noreply.github.com>
2020-03-29 15:51:03 -0500

    imported material ui, and minor changes to register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,43 +1,87 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
+import { TextField } from "@material-ui/core";
 
-const Register = (props) => {
+function Register() {
+  const [registerInfo, setRegisterInfo] = useState([]);
 
-  const [registerInfo, setRegisterInfo] = useState([])
-
-  const handleChange = (e) => {
+  const handleChange = e => {
     setRegisterInfo({
       ...registerInfo,
       [e.target.name]: e.target.value
-    })
-  }
+    });
+  };
 
   const onHandleRegister = () => {
-    fetch('http://localhost:3301/add-user', {
-      method: 'POST',
+    fetch("http://localhost:3301/add-user", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
-    },
+        "Content-Type": "application/json"
+      },
       body: JSON.stringify(registerInfo)
-    }).then(response => response.json())
-  }
+    }).then(response => response.json());
+  };
 
-    return (<>
-    
-        <div className="row">
-          <div className="col">
-            <h1>Register</h1>
-            <p><i>...and start bud getting today!</i></p>
-            <input type="text" onChange={handleChange} name="first" placeholder="first name" />
-            <input type="text" onChange={handleChange} name="last" placeholder="last name" />
-            <input type="text" onChange={handleChange} name="display_name" placeholder="what name do you prefer to go by?" />
-            <input type="email" onChange={handleChange} name="email" placeholder="email" />
-            <input type="text" onChange={handleChange} name="phone_number" placeholder="phone number" />
-            <input type="password" onChange={handleChange} name="password" placeholder="password" />
-            <button onClick={onHandleRegister}>Register</button>
-          </div>
+  return (
+    <>
+      <div className="row">
+        <div className="col">
+          <h1>Register</h1>
+          <p>
+            <i>...and start bud getting today!</i>
+          </p>
+          <TextField
+            type="text"
+            onChange={handleChange}
+            name="first"
+            fullWidth
+            id="standard-basic"
+            label="first name"
+          />
+          <TextField
+            type="text"
+            onChange={handleChange}
+            name="last"
+            fullWidth
+            id="standard-basic"
+            label="last name"
+          />
+          <TextField
+            type="text"
+            onChange={handleChange}
+            name="display_name"
+            fullWidth
+            id="standard-basic"
+            label="what name do you prefer to go by?"
+          />
+          <TextField
+            type="email"
+            onChange={handleChange}
+            name="email"
+            fullWidth
+            id="standard-basic"
+            label="email"
+          />
+          <TextField
+            type="text"
+            onChange={handleChange}
+            name="phone_number"
+            fullWidth
+            id="standard-basic"
+            label="phone number"
+          />
+          <TextField
+            type="password"
+            onChange={handleChange}
+            name="password"
+            fullWidth
+            id="standard-basic"
+            label="password"
+          />
+          <button onClick={onHandleRegister}>Register</button>
         </div>
-
-    </>)
+      </div>
+    </>
+  );
 }
 
-export default Register
+export default Register;
